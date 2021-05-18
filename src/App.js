ade90ae79ba1ca646490186789187298f96d3aa9
miguel787 <miguelraulcruz@gmail.com>
2021-05-18 16:13:26 -0400

    project completed

--- src/App.js
+++ src/App.js
@@ -41,7 +41,7 @@ function App() {
     if (localStorage.getItem("todos") === null) {
       localStorage.setItem("todos", JSON.stringify([]));
     } else {
-      let todoLocal = JSON.parse(localStorage.getItem(todos));
+      let todoLocal = JSON.parse(localStorage.getItem("todos"));
       setTodos(todoLocal);
     }
   };

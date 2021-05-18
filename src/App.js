c4e283e33c326bff11a55bb1c5d7d19ff0bf6f4e
miguel787 <miguelraulcruz@gmail.com>
2021-05-17 23:22:05 -0400

    started working on state adding text functionality

--- src/App.js
+++ src/App.js
@@ -5,13 +5,19 @@ import TodoList from "./components/TodoList";
 
 function App() {
   const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
   return (
     <div className="App">
       <header>
         <h1>Miguel's Todo List</h1>
       </header>
-      <Form />
-      <TodoList />
+      <Form
+        inputText={inputText}
+        todos={todos}
+        setTodos={setTodos}
+        setInputText={setInputText}
+      />
+      <TodoList todos={todos} />
     </div>
   );
 }

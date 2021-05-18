a0fd145cb1381c149e8153779dffa83d8e24a291
miguel787 <miguelraulcruz@gmail.com>
2021-05-18 03:28:30 -0400

    almost completed all thats left is the nasty error with local storage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,50 @@
 import "./App.css";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Form from "./components/Form";
 import TodoList from "./components/TodoList";
 
 function App() {
+  //states
   const [inputText, setInputText] = useState("");
   const [todos, setTodos] = useState([]);
+  const [status, setStatus] = useState("all");
+  const [filteredTodos, setFilteredTodos] = useState([]);
+  //functions
+  const filterHandler = () => {
+    switch (status) {
+      case "completed":
+        setFilteredTodos(todos.filter((todo) => todo.completed === true));
+        break;
+      case "uncompleted":
+        setFilteredTodos(todos.filter((todo) => todo.completed === false));
+        break;
+      default:
+        setFilteredTodos(todos);
+        break;
+    }
+  };
+  //runs once
+  useEffect(() => {
+    getLocalTodos();
+  }, []);
+
+  useEffect(() => {
+    filterHandler();
+    saveLocalTodos();
+  }, [todos, status]);
+  //Save Locally
+
+  const saveLocalTodos = () => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  };
+  const getLocalTodos = () => {
+    if (localStorage.getItem("todos") === null) {
+      localStorage.setItem("todos", JSON.stringify([]));
+    } else {
+      let todoLocal = JSON.parse(localStorage.getItem(todos));
+      setTodos(todoLocal);
+    }
+  };
   return (
     <div className="App">
       <header>
@@ -16,8 +55,13 @@ function App() {
         todos={todos}
         setTodos={setTodos}
         setInputText={setInputText}
+        setStatus={setStatus}
       />
-      <TodoList todos={todos} />
+      <TodoList
+        todos={todos}
+        setTodos={setTodos}
+        filteredTodos={filteredTodos}
+      />
     </div>
   );
 }
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import Todo from "./Todo";
-const TodoList = ({ todos }) => {
+const TodoList = ({ todos, setTodos, filteredTodos }) => {
   return (
     <div className="todo-container">
       <ul className="todo-list">
-        {todos.map((todo) => (
-          <Todo text={todo.text} id={todo.id} />
+        {filteredTodos.map((todo) => (
+          <Todo
+            todos={todos}
+            setTodos={setTodos}
+            todo={todo}
+            key={todo.id}
+            text={todo.text}
+          />
         ))}
       </ul>
     </div>
